47b2bc98524b0ebdba9fa0a15ec6e1dacd0b88c7
Madina-88 <ergasevamalika066.gmail.com>
2023-10-11 18:16:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        /*int num1 = scanner.nextInt();
+        int num2 = 4;
+        int num3 = 8;
+        if (num2>num3){
+        System.out.println("чон");
+        }else {
+        System.out.println("кичине");
+        }*/
+
+
+        /*int num6 = scanner.nextInt();
+        if (num6 < 18) {
+            System.out.println("жетпеди");
+        } else {
+            System.out.println("Жетти");*/
+
+        /*Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        switch (str) {
+            case "USD":
+                System.out.println("84.5");
+                break;
+            case "EUR":
+                System.out.println("102/58");
+                break;
+            case "RUB":
+                System.out.println("1/15");
+                break;
+            case "KZT":
+                System.out.println(0.20);
+                break;*/
+
+
+        /*Scanner sc = new Scanner(System.in);
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        int num5 = scanner.nextInt();
+        int num6 = scanner.nextInt();
+        if (num4 > num5) {
+            System.out.println(num3);
+        } else if (num4 < num5) {
+            System.out.println("num4");
+        } else if (num5 < num6) {
+            System.out.println("num5");
+        } else {
+            System.out.println("num6");*/
+        }
+    }
+
+
+
+
+
+
